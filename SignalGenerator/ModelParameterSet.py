094b2a0f62449f84fc0c3b64004b6586b9fc7593
Tristan West <tristan.west@tessella.com>
2016-04-18 22:45:02 +0100

    Create stub for parameter set class

---
+++ SignalGenerator/ModelParameterSet.py
@@ -0,0 +1,9 @@
+class ModelParameterSet:
+    """A parameter set for a coupled ion model."""
+    def __init__(self):
+        self.Delta = 0
+        self.Gamma = 0
+        self.gamma = 0
+        self.J = 0
+        self.Omega_1 = 0
+        self.Omega_2 = 0
